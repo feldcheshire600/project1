81c846d88ba8946fb8a534e5b5217096dccee156
feldcheshire600 <feldcheshire600@gmail.com>
2025-04-02 04:15:50 +0000

    Add 23.php

---
+++ src/23.php
@@ -0,0 +1,12 @@
+<?php
+  function echo_random_string($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+      $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $randomString;
+  }
+
+  echo_random_string(20);
+?>
