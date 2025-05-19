640525de46729299afeb1ece17264ff2d5f44b13
feldcheshire600 <feldcheshire600@gmail.com>
2025-05-19 10:58:11 +0000

    Add 37.php

---
+++ src/37.php
@@ -0,0 +1,14 @@
+<?php
+// This is a sample PHP code to demonstrate basic variable usage and control structures.
+echo "Hello, World!"; // Output: Hello, World!
+$a = 5;
+$b = $a * 2; // $b will be 10
+echo "The value of b is: " . $b;
+
+if ($a > 10) {
+    echo "The number in the if statement is greater than 10.";
+}
+else {
+    echo "The number in the else statement is not greater than 10.";
+}
+?>
